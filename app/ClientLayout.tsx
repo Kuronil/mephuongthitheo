d4921511111e8260700e3fccbbe9ee320bc5e5f7
Kuronil <takamon654@gmail.com>
2025-11-01 12:13:11 +0700

    capnhat 1.2

--- app/ClientLayout.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { UserProvider } from "@/contexts/UserContext";
-
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  return <UserProvider>{children}</UserProvider>;
-}
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,7 +14,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/checkout',
     '/loyalty',
     '/order-tracking',
-    '/store-location-demo',
   ]
 
   const now = new Date()
